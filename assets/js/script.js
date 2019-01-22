208634f5f9a9984c66639c30e29a4959e85e7173
DELHAY <delhayantoine@gmail.com>
2019-01-22 08:36:26 +0100

    Ajout de l'exercice 6

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  document.getElementById("myImg1").src = "assets/images/image1_2.jpg";
+  document.getElementById("myImg2").src = "assets/images/image2_2.jpg";
+  document.getElementById("myImg3").src = "assets/images/image3_2.jpg";
+  document.getElementById("myImg4").src = "assets/images/image4_2.jpg";
+  document.getElementById("myImg5").src = "assets/images/image5_2.jpg";
+}
+
+function myFunction2() {
+  document.getElementById("myImg1").src = "assets/images/image1.jpg";
+  document.getElementById("myImg2").src = "assets/images/image2.jpg";
+  document.getElementById("myImg3").src = "assets/images/image3.jpg";
+  document.getElementById("myImg4").src = "assets/images/image4.jpg";
+  document.getElementById("myImg5").src = "assets/images/image5.jpg";
+}
